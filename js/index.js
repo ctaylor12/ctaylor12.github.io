f9aac90bd7fba343317078918bbee0603e3a470e
ctaylor12 <digi_freek@Chelseas-MacBook-Pro.local>
2016-03-18 22:14:14 -0700

    more collagePlus

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(window).load(function () {
+        $('.Collage').collagePlus();
+    });
+
+$('.collage').collagePlus('allowPartialLastRow' : true);
+
+$('.collage').collagePlus(
+        {
+            'effect' : 'effect-3',
+        }
+    );
